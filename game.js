11d67b2590f3ca7c7dc5ec27224e1f27e27f84c6
Bruno Fernandes <bwendel26999@gmail.com>
2020-04-03 20:40:56 -0300

    ajustando fluxo de derrota e vitoria do game, criação do doc fim_de_jogo.html, e ajustes para para inserir o cronometro do game e o contador de vidas.

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 var altura = 0;
 var largura = 0;
+var vidas = 1;
+var tempo = 10;
 
 function verificaTamTela() {
    //pegando as proporcoes do navegador
@@ -10,6 +12,22 @@ function verificaTamTela() {
 }
 verificaTamTela(); //retorna as proporcoes da tela no console
 
+//Controle de tempo do jogo
+var cronometro = setInterval(() => {
+   tempo -= 1;
+   //ajuste para nao prosseguir com nums negativos
+   if(tempo < 0) {
+      //parar o fluxo do interval
+      clearInterval(cronometro);
+      clearInterval(criaMosquito);
+      window.location.href = 'vitoria.html';
+   } else {
+      //modificando o valor contido entre a tag
+      document.getElementById('tempo').innerHTML = tempo;
+   }
+}, 1000)
+
+/////////////
 
 function tamanhoAleatorio() {
    var classe = Math.floor(Math.random() * 3);
@@ -40,6 +58,14 @@ function posRandom() {
    //remover mosquito anterior caso exista
    if(document.getElementById('mosquito')){ //se retornar null é false
       document.getElementById('mosquito').remove() //removendo mosquito
+      //controle da vida
+      if(vidas> 3){
+         //redirecionamento de página:
+         window.location.href = 'fim_de_jogo.html';
+      }else {    
+         document.getElementById(`v${vidas}`).src = 'imgs/coracao_vazio.png';
+         vidas++;
+      }
    }
 
    var posX = Math.floor(Math.random() * largura) - 60;
@@ -62,14 +88,11 @@ function posRandom() {
    mosquito.style.left = `${posX}px`;
    mosquito.style.top = `${posY}px`;
    mosquito.id = 'mosquito'; //definindo id para logica de inserção/remoção
+   mosquito.onclick = function() {
+      this.remove(); //faz referencia ao proprio elemento
+      //se o elemento for clicado antes ele é removido e nao entra 
+      //no fluxo da funcao posRandom()
+   }
 
    document.body.appendChild(mosquito);
-
-
 }
-
-//executando a chamada de posRandom em 1200 milisecs
-setInterval(function() {
-   posRandom();
-}, 1000)
-
